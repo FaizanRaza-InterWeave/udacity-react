e53057e379de1acee4fc33bae360de3c10942deb
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-07 18:52:50 +0000

    add bootstrap

--- React-Fundamentals-Exercises/L3. State Management/5. Controlled Components II/Starter/src/App.js
+++ React-Fundamentals-Exercises/L3. State Management/5. Controlled Components II/Starter/src/App.js
@@ -3,47 +3,23 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
-  const [value, setValue] = useState("");
   const [items, setItems] = useState([]);
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  const addItem = (event) => {
-    event.preventDefault();
-    setItems([...items, value]);
-    setValue("");
-  };
 
   const deleteLastItem = (event) => {
     setItems(items.slice(0, -1));
   };
 
-  const inputIsEmpty = () => value === "";
-
   const noItemsFound = () => items.length === 0;
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">ReactND - Coding Practice</h1>
-      </header>
-      <h2>Shopping List</h2>
-      <form onSubmit={addItem}>
-        <input
-          type="text"
-          placeholder="Enter New Item"
-          value={value}
-          onChange={handleChange}
-        />
-        <button disabled={inputIsEmpty()}>Add</button>
-      </form>
-
-      <button onClick={deleteLastItem} disabled={noItemsFound()}>
-        Delete Last Item
-      </button>
+      <Header />
+      <Form
+        deleteLastItem={deleteLastItem}
+        noItemsFound={noItemsFound}
+        setItems={setItems}
+        items={items}
+      />
 
       <ItemsList items={items} />
     </div>
@@ -61,6 +37,46 @@ const ItemsList = ({ items }) => {
           <li key={index}>{item}</li>
         ))}
       </ol>
+    </>
+  );
+};
+
+const Header = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">ReactND - Coding Practice</h1>
+  </header>
+);
+
+const Form = ({ items, setItems, deleteLastItem, noItemsFound }) => {
+  const [value, setValue] = useState("");
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const addItem = (event) => {
+    event.preventDefault();
+    setItems([...items, value]);
+    setValue("");
+  };
+
+  const inputIsEmpty = () => value === "";
+
+  return (
+    <>
+      <h2>Shopping List</h2>
+      <form onSubmit={addItem}>
+        <input
+          type="text"
+          placeholder="Enter New Item"
+          value={value}
+          onChange={handleChange}
+        />
+        <button disabled={inputIsEmpty()}>Add</button>
+      </form>
+      <button onClick={deleteLastItem} disabled={noItemsFound()}>
+        Delete Last Item
+      </button>
     </>
   );
 };
