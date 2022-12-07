0ef52a91bf70d29b38294961d7bbf9fd7e8fba34
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-07 20:13:08 +0000

    add controlled switch

--- React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/components/GamesPerUserTable.js
+++ React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/components/GamesPerUserTable.js
@@ -1,18 +1,50 @@
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 import { ListUser } from "./ListItem";
+import { useState } from "react";
 
 export const GamesPerUserTable = () => {
+  const [showGamesPlayed, setShowGamesPlayed] = useState(false);
   return (
     <>
       <div className="row justify-content-center">
         <h2>Games Per User</h2>
       </div>
       <div className="row justify-content-center">
-        <ul class="list-group col-6">
+        <ul className="list-group col-6">
           <ListUser username="musky" gamesPlayed="1" />
           <ListUser username="ball" gamesPlayed="2" />
           <ListUser username="sweat" gamesPlayed="3" />
         </ul>
       </div>
+      <Row className="row justify-content-center">
+        <Col>
+          <SwitchExample
+            setShowGamesPlayed={setShowGamesPlayed}
+            showGamesPlayed={showGamesPlayed}
+          />
+        </Col>
+      </Row>
     </>
   );
 };
+
+function SwitchExample({ showGamesPlayed, setShowGamesPlayed }) {
+  return (
+    <Form className="row justify-content-center align-items-left float-left">
+      <Col xs="auto" className="my-1">
+        <Form.Check
+          type="switch"
+          id="custom-switch"
+          label="Show Games Played"
+          onChange={(event) => {
+            setShowGamesPlayed(event.target.checked);
+          }}
+          checked={showGamesPlayed}
+        />
+      </Col>
+    </Form>
+  );
+}
