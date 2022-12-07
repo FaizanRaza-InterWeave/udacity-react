aaa4f33221aa67f61964e91f7d1a4cb7462e759f
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-07 19:12:29 +0000

    add form HTML

--- React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/App.js
+++ React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/App.js
@@ -3,12 +3,58 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">ReactND - Coding Practice</h1>
-      </header>
-    </div>
+    <>
+      <div class="container-fluid-lg">
+        <div className="App row">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">ReactND - Coding Practice</h1>
+          </header>
+          <body>
+            <div class="container-lg">
+              <div className="row justify-content-start">
+                <div className="row">
+                  <h2>Add User</h2>
+                </div>
+                <div className="row">
+                  <div class="col-4">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="First name"
+                      aria-label="First name"
+                    ></input>
+                  </div>
+                  <div class="col-4">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Last name"
+                      aria-label="Last name"
+                    ></input>
+                  </div>
+                  <div class="col-4">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Username"
+                      aria-label="Username"
+                    ></input>
+                  </div>
+                </div>
+              </div>
+              <div class="row justify-content-end mt-3">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                  <button class="btn btn-primary me-md-2" type="button">
+                    Button
+                  </button>
+                </div>
+              </div>
+            </div>
+          </body>
+        </div>
+      </div>
+    </>
   );
 };
 
