45475f9e5dc7a33f925fda47fdd024fb06413e9d
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-08 17:14:45 +0000

    Add ability to add users to list

--- React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/App.js
+++ React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/App.js
@@ -6,9 +6,24 @@ import { useState } from "react";
 
 const App = () => {
   const [users, setUsers] = useState([
-    { firstName: "Musky", lastName: "Balls", userName: "MuskyBalls" },
-    { firstName: "Sweaty", lastName: "Crack", userName: "SweatyCrack" },
-    { firstName: "Wiz", lastName: "Queefa", userName: "WizQueefa" },
+    {
+      firstName: "Musky",
+      lastName: "Balls",
+      userName: "MuskyBalls",
+      gamesPlayed: 1,
+    },
+    {
+      firstName: "Sweaty",
+      lastName: "Crack",
+      userName: "SweatyCrack",
+      gamesPlayed: 2,
+    },
+    {
+      firstName: "Wiz",
+      lastName: "Queefa",
+      userName: "WizQueefa",
+      gamesPlayed: 3,
+    },
   ]);
   return (
     <>
@@ -21,7 +36,7 @@ const App = () => {
           <body>
             <div class="container-lg">
               <div className="row justify-content-start">
-                <AddUser setUsers={setUsers} />
+                <AddUser users={users} setUsers={setUsers} />
               </div>
               <div class="row justify-content-end mt-3">
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end">
--- React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/components/AddUser.js
+++ React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/components/AddUser.js
@@ -2,8 +2,13 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/esm/Row";
+import { useState } from "react";
 
-export const AddUser = () => {
+export const AddUser = ({ users, setUsers }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+
   return (
     <>
       <Row>
@@ -13,22 +18,58 @@ export const AddUser = () => {
         <Row>
           <Col>
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Control type="text" placeholder="First name" />
+              <Form.Control
+                type="text"
+                placeholder="First name"
+                value={firstName}
+                onChange={(event) => {
+                  setFirstName(event.target.value);
+                }}
+              />
             </Form.Group>
           </Col>
           <Col>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicPassword"
+              value={lastName}
+              onChange={(event) => {
+                setLastName(event.target.value);
+              }}
+            >
               <Form.Control type="text" placeholder="Last name" />
             </Form.Group>
           </Col>{" "}
           <Col>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicPassword"
+              value={userName}
+              onChange={(event) => {
+                setUserName(event.target.value);
+              }}
+            >
               <Form.Control type="text" placeholder="Username" />
             </Form.Group>
           </Col>
         </Row>
 
-        <Button variant="primary" type="submit">
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(event) => {
+            event.preventDefault();
+            setUsers([
+              ...users,
+              {
+                firstName,
+                lastName,
+                userName,
+                gamesPlayed: 0,
+              },
+            ]);
+          }}
+        >
           Submit
         </Button>
       </Form>
