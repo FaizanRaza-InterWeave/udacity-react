4b801c6958aae9b35c7af1b09ec92f4505dae590
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-10 12:49:47 +0000

    modify message into stateful component

--- React-Fundamentals-Exercises/L4. Hooks/1. State Management Recap/Starter/src/Components/ChatWindow.js
+++ React-Fundamentals-Exercises/L4. Hooks/1. State Management Recap/Starter/src/Components/ChatWindow.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export const ChatWindow = ({ users, isDisabled, messages }) => {
+  const [message, setMessage] = useState("");
   return (
     <div className="chat-window">
       <h2>Super Awesome Chat</h2>
@@ -24,6 +27,10 @@ export const ChatWindow = ({ users, isDisabled, messages }) => {
             type="text"
             className="form-control"
             placeholder="Enter your message..."
+            value={message}
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
           />
           <div className="input-group-append">
             <button className="btn submit-button" disabled={isDisabled()}>
