047d86943241d260b713e4a94954ec39b14aae82
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-10 12:47:55 +0000

    separate out chat Window

--- React-Fundamentals-Exercises/L4. Hooks/1. State Management Recap/Starter/src/App.js
+++ React-Fundamentals-Exercises/L4. Hooks/1. State Management Recap/Starter/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { ChatWindow } from "./Components/ChatWindow";
 
 const users = [{ username: "Amy" }, { username: "John" }];
 
@@ -22,74 +23,7 @@ const App = () => {
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
       <div className="container">
-        <div className="chat-window">
-          <h2>Super Awesome Chat</h2>
-          <div className="name sender">{users[0].username}</div>
-
-          <ul className="message-list">
-            {messages.map((message, index) => (
-              <li
-                key={index}
-                className={
-                  message.username === users[0].username
-                    ? "message sender"
-                    : "message recipient"
-                }
-              >
-                <p>{`${message.username}: ${message.text}`}</p>
-              </li>
-            ))}
-          </ul>
-
-          <div>
-            <form className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your message..."
-              />
-              <div className="input-group-append">
-                <button className="btn submit-button" disabled={isDisabled()}>
-                  SEND
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-
-        <div className="chat-window">
-          <h2>Super Awesome Chat</h2>
-          <div className="name sender">{users[1].username}</div>
-          <ul className="message-list">
-            {messages.map((message, index) => (
-              <li
-                key={index}
-                className={
-                  message.username === users[1].username
-                    ? "message sender"
-                    : "message recipient"
-                }
-              >
-                <p>{`${message.username}: ${message.text}`}</p>
-              </li>
-            ))}
-          </ul>
-
-          <div>
-            <form className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your message..."
-              />
-              <div className="input-group-append">
-                <button className="btn submit-button" disabled={isDisabled()}>
-                  SEND
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <ChatWindow users={users} messages={messages} isDisabled={isDisabled} />
       </div>
     </div>
   );
---
+++ React-Fundamentals-Exercises/L4. Hooks/1. State Management Recap/Starter/src/Components/ChatWindow.js
@@ -0,0 +1,37 @@
+export const ChatWindow = ({ users, isDisabled, messages }) => {
+  return (
+    <div className="chat-window">
+      <h2>Super Awesome Chat</h2>
+      <div className="name sender">{users[1].username}</div>
+      <ul className="message-list">
+        {messages.map((message, index) => (
+          <li
+            key={index}
+            className={
+              message.username === users[1].username
+                ? "message sender"
+                : "message recipient"
+            }
+          >
+            <p>{`${message.username}: ${message.text}`}</p>
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <form className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter your message..."
+          />
+          <div className="input-group-append">
+            <button className="btn submit-button" disabled={isDisabled()}>
+              SEND
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
