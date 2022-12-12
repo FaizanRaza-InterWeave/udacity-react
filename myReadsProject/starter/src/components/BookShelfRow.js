2e7fea5bae4f18215057633a16bc7d8224dfa3bc
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-12 15:04:26 +0000

    add change shelf to readable text

--- myReadsProject/starter/src/components/BookShelfRow.js
+++ myReadsProject/starter/src/components/BookShelfRow.js
@@ -2,9 +2,14 @@ import { Book } from "./Book";
 
 export const BookShelfRow = ({ books, rowName, setBooks }) => {
   const booksToDisplay = books.filter((book) => book.shelf === rowName);
+  const shelfText = {
+    currentlyReading: "Currently Reading",
+    wantToRead: "Want to Read",
+    read: "Read",
+  };
   return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">{rowName}</h2>
+      <h2 className="bookshelf-title">{shelfText[rowName]}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           {booksToDisplay.map((book) => (
