e9120611a68468564f3162f6d84a9f473754e707
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-18 22:08:07 +0000

    create Goals component

--- demo_create/src/Components/Goals.js
+++ demo_create/src/Components/Goals.js
@@ -1,8 +1,27 @@
+import { useRef } from "react";
 import { List } from "./List";
-export const Goals = () => {
+import { addGoalAction, generateId } from "./store";
+
+export const Goals = ({ store }) => {
+  const inputRef = useRef();
+
+  const addItem = (event) => {
+    event.preventDefault();
+    const name = inputRef.current.value;
+    inputRef.current.value = "";
+
+    store.dispatch(
+      addGoalAction({
+        name,
+        id: generateId(),
+      })
+    );
+  };
   return (
     <div>
-      Goals
+      <h1>Goals</h1>
+      <input type="text" placeholder="Add Goal" ref={inputRef} />
+      <button onClick={addItem}>Add Goal</button>
       <List />
     </div>
   );
