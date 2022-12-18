003846f25401c71af2db3c29aba9e7ff298c1cfd
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-18 14:20:07 +0000

    add basic addGoal addTodos UI

--- demo_create/src/App.js
+++ demo_create/src/App.js
@@ -3,7 +3,18 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
-      <p>test </p>
+      <div>
+        <h1> Todo List</h1>
+        <input id="todo" type="text" placeholder="Add Todo" />
+        <button id="todoBtn">Add Todo</button>
+        <ul id="todos"></ul>
+      </div>
+      <div>
+        <h1> Goals List</h1>
+        <input id="goal" type="text" placeholder="Add Goal" />
+        <button id="goalBtn">Add Goal</button>
+        <ul id="goals"></ul>
+      </div>
     </div>
   );
 }
