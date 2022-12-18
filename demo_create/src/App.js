3fde1bf073784758992183945041a696c836c0b1
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-18 15:00:08 +0000

    align with end of UI

--- demo_create/src/App.js
+++ demo_create/src/App.js
@@ -1,22 +1,7 @@
 import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h1> Todo List</h1>
-        <input id="todo" type="text" placeholder="Add Todo" />
-        <button id="todoBtn">Add Todo</button>
-        <ul id="todos"></ul>
-      </div>
-      <div>
-        <h1> Goals List</h1>
-        <input id="goal" type="text" placeholder="Add Goal" />
-        <button id="goalBtn">Add Goal</button>
-        <ul id="goals"></ul>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
