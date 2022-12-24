d35046fa97f5079df4b4b39b6d5b140cdb528dfe
Faizan Raza <faizan.raza@infinityworks.com>
2022-12-24 21:30:49 +0000

    complete tweets page

--- react-chirper-app/src/components/App/index.js
+++ react-chirper-app/src/components/App/index.js
@@ -4,16 +4,23 @@ import { handleInitialData } from "../Shared/initialData";
 import { Dashboard } from "../Dashboard";
 import { NewTweet } from "../NewTweet";
 import LoadingBar from "react-redux-loading-bar";
+import { TweetPage } from "../../pages/Tweet";
 
 const App = ({ dispatch, loading }) => {
   useEffect(() => {
     dispatch(handleInitialData());
   }, []);
+  console.log(loading);
   return (
     <>
       <LoadingBar />
       {/* <div>{loading ? null : <Dashboard />}</div> */}
-      <div>{loading ? null : <NewTweet />}</div>
+      {/* <div>{loading ? null : <NewTweet />}</div> */}
+      <div>
+        {loading ? null : (
+          <TweetPage match={{ params: { id: "8xf0y6ziyjabvozdd253nd" } }} />
+        )}
+      </div>
     </>
   );
 };
